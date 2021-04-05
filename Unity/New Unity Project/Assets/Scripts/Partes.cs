23de07cc39ce2202b423e51611c0cfd92a0da6d8
Gabriel Mendes <81168814+GabrielMendesM@users.noreply.github.com>
2021-04-05 12:11:09 -0300

    upload Projeto Unity

---
+++ Unity/New Unity Project/Assets/Scripts/Partes.cs
@@ -0,0 +1,43 @@
+/*
+public abstract class Partes {
+    private enum PARTES {
+        BRACOS,
+        CORPO,
+        BICO
+    }
+
+    public static bool bracos { get; private set; }
+    public static bool bico { get; private set; }
+
+    public static bool Mudar(PARTES parte)
+    {
+        switch (parte)
+        {
+            case PARTES.BICO
+        }
+    }
+    public static bool MudarBraco(int bracosId)
+    {
+        if (bracosId == 0)
+        {
+            return bracos = false;
+        }
+        else
+        {
+            return bracos = true;
+        }
+    }
+
+    public static bool MudarBico()
+    {
+        if (bracosId == 0)
+        {
+            return bracos = false;
+        }
+        else
+        {
+            return bracos = true;
+        }
+    }
+}
+*/
