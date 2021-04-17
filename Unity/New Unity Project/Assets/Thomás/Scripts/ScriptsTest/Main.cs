a533f27c2bbe7d0d91efdb432d7d13f3f5adac5f
THOMAS PEREIRA CASTRO MAYRINK <thomasmayrink@gmail.com>
2021-04-17 01:04:36 -0300

    Organizando
    Organizando cenas e interfaces

--- Unity/New Unity Project/Assets/Thomás/Scripts/ScriptsTest/Main.cs
+++ Unity/New Unity Project/Assets/Thomás/Scripts/ScriptsTest/Main.cs
@@ -6,7 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour
 {    
-
+    public GameObject tiGO;
+    public GameObject comecarGO;
+    public void ComeçarJogo()
+    {
+        tiGO.gameObject.SetActive(false);
+        comecarGO.gameObject.SetActive(true);
+    }
     public void PlayButton()
     {
         SceneManager.LoadScene("WhackAMole");
