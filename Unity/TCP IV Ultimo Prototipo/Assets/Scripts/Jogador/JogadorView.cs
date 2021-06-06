30e9555ec815bef3bd58dba54934cd0ed5922f3b
Gabriel Mendes <81168814+GabrielMendesM@users.noreply.github.com>
2021-06-06 15:34:53 -0300

    correção em luz e menu

--- Unity/TCP IV Ultimo Prototipo/Assets/Scripts/Jogador/JogadorView.cs
+++ Unity/TCP IV Ultimo Prototipo/Assets/Scripts/Jogador/JogadorView.cs
@@ -37,7 +37,7 @@ public class JogadorView : Elemento
             case Estados.Rodando:
                 if (timer >= 0.05f)
                 {
-                    Dano(1);
+                    Dano(0);
                     efeitoDano = false;
                     timer = 0;
                 }
